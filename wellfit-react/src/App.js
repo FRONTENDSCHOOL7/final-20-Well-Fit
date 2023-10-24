917e1331ab8ffc27157fcc566ea49be4841d7038
swJaNG12 <cbdjej38483@gmail.com>
2023-10-24 14:23:52 +0900

    💄 PageHome 컴포넌트 구현

--- wellfit-react/src/App.js
+++ wellfit-react/src/App.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from '../src/resetcss/reset.css';
-import Footer from './Components/common/Footer/Footer';
-import MainHeader from './Components/common/Header/MainHeader';
+import PageHome from './Pages/Home/PageHome';
 
 const GlobalStyle = createGlobalStyle`
 
@@ -26,8 +25,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <MainHeader />
-      <Footer />
+      <PageHome />
     </>
   );
 }
--- wellfit-react/src/Pages/Home/PageHome.jsx
+++ wellfit-react/src/Pages/Home/PageHome.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import HomeHeader from '../../Components/Home/HomeHeader';
+import { styled } from 'styled-components';
+import HomeContent from '../../Components/Home/HomeContent';
 
-export default function Page_Home() {
-  return <div>Page_Home</div>;
+const StyledHomePage = styled.div`
+  width: 390px;
+  height: 820px;
+  margin: 0 auto;
+  border: 1px solid #dbdbdb;
+
+  /* 게시물 많아지면 스크롤 생성 */
+  overflow: hidden;
+`;
+
+export default function PageHome() {
+  return (
+    <StyledHomePage>
+      <HomeHeader />
+      <HomeContent />
+    </StyledHomePage>
+  );
 }
