9115e20c9343392866b869264dadf131eba1491e
SMJ426 <jerry04260@gmail.com>
2023-10-24 11:13:44 +0900

    💄styled-component 로 컴포넌트 구현 작업 툴 완료
    개발 시작

---
+++ wellfit-react/src/Pages/Home/Page_Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Page_Home() {
+  return <div>Page_Home</div>;
+}
---
+++ wellfit-react/src/Pages/Signup/Page_Signup.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Page_Signup() {
+  return <div>Page_Signup</div>;
+}
