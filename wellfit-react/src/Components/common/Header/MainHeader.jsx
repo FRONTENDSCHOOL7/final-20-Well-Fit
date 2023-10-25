e0b5cdf681c5d47eb9b3e1665a4a1a6dceea7a16
swJaNG12 <cbdjej38483@gmail.com>
2023-10-25 17:37:12 +0900

    📝 MainHeader, 공통 Footer 이미지 경로 수정

--- wellfit-react/src/Components/common/Header/MainHeader.jsx
+++ wellfit-react/src/Components/common/Header/MainHeader.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import gobackIcon from '../../../images/icon-arrow-left.svg';
+import modrVerticalIcon from '../../../images/icon- more-vertical.svg';
 
-const StyledMainHeader = styled.header`
+const StyledChattingUserListHeader = styled.header`
   border-bottom: 1px solid #dbdbdb;
 
   & .search-bar {
@@ -10,33 +12,32 @@ const StyledMainHeader = styled.header`
     align-items: center;
     gap: 6px;
     padding: 13px 12px 13px 16px;
-    background-color: #fefefe;
   }
   & .search-bar .btn-goback {
     width: 22px;
     height: 22px;
-    background-image: url('../../../../images/icon-arrow-left.svg');
     vertical-align: middle;
   }
   & .search-bar .btn-toggle {
     width: 24px;
     height: 24px;
-    background-image: url('../../../../images/icon-\ more-vertical.svg');
     vertical-align: middle;
   }
 `;
 
-export default function MainHeader() {
+export default function ChattingUserListHeader() {
   return (
-    <StyledMainHeader>
+    <StyledChattingUserListHeader>
       <div className="search-bar">
         <button type="button" className="btn-goback">
+          <img src={gobackIcon} alt="뒤로가기버튼" />
           <span className="a11y-hidden">뒤로가기</span>
         </button>
-        <button type="button" className="btn-toggle">
+        <button type="button" class="btn-toggle">
+          <img src={modrVerticalIcon} alt="토글" />
           <span className="a11y-hidden">토글</span>
         </button>
       </div>
-    </StyledMainHeader>
+    </StyledChattingUserListHeader>
   );
 }
