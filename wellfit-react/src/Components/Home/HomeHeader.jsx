8ac74cf11301178a52ef1c6180eabb66f724f5a8
swJaNG12 <cbdjej38483@gmail.com>
2023-10-29 22:16:33 +0900

    💄 HomeHeader에 검색아이콘, 검색창이동 추가

--- wellfit-react/src/Components/Home/HomeHeader.jsx
+++ wellfit-react/src/Components/Home/HomeHeader.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import searchIcon from '../../images/icon-search.svg';
+import { useNavigate } from 'react-router-dom';
 
 const StyledHomeHeader = styled.header`
   font-size: 18px;
   font-weight: bold;
-  padding: 13px 0 13px 16px;
+  padding: 13px 16px 13px 16px;
   border-bottom: 1px solid #dbdbdb;
+  display: flex;
+  justify-content: space-between;
+
+  & .btn-search {
+    width: 24px;
+    height: 24px;
+  }
 `;
 
 export default function HomeHeader() {
+  const navigate = useNavigate();
+  const gosearch = () => {
+    navigate('/home/search');
+  };
   return (
     <>
       <StyledHomeHeader>
-        <p>감귤마켓 피드</p>
+        <p className="home-title">감귤마켓 피드</p>
+        <button className="btn-search" type="button" onClick={gosearch}>
+          <img src={searchIcon} alt="검색" />
+        </button>
       </StyledHomeHeader>
     </>
   );
