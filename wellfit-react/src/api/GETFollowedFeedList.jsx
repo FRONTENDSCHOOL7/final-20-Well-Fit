6c4391a1a469daabe9f60462d1451b8931b6da9e
swJaNG12 <cbdjej38483@gmail.com>
2023-10-30 17:53:50 +0900

    🗂️ 팔로우한 유저들의 게시물 리스트 받아오기 인스턴스

---
+++ wellfit-react/src/api/GETFollowedFeedList.jsx
@@ -0,0 +1,15 @@
+import { authInstance } from './instance';
+
+export const getFollowedUserFeedList = async () => {
+  const URL = 'post/feed';
+
+  try {
+    const response = await authInstance.get(URL);
+    if (response.data.status === 404 || response.status === 404) {
+      throw new Error('404 에러');
+    }
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
