03e748fa9b9c1eea0d310b7920ac69eb20ff2d5c
swJaNG12 <cbdjej38483@gmail.com>
2023-10-30 14:25:51 +0900

    🗂️ add:  내 팔로우 리스트 불러오기 인스턴스

---
+++ wellfit-react/src/api/PostMyInfo.jsx
@@ -0,0 +1,17 @@
+import { tokenInstance } from './instance';
+
+// 내 프로필 정보 받아오기
+const MY_INFO_URL = 'user/myinfo';
+
+export const getMyInfo = async () => {
+  try {
+    const response = await tokenInstance.get(MY_INFO_URL);
+    if (response.status === 404) {
+      throw new Error('404 에러');
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
