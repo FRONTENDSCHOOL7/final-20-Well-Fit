a838364f1658c39930b91754a833f605e3932e52
yeslcho <skyblueys123@gmail.com>
2023-10-30 09:15:25 +0900

    📝 App.js 수정

--- wellfit-react/src/App.js
+++ wellfit-react/src/App.js
@@ -1,9 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from '../src/resetcss/reset.css';
-import Footer from './Components/common/Footer/Footer';
-import PageUsersProfile from './Pages/Profile/PageUsersProfile';
-import PageMineProfile from './Pages/Profile/PageMineProfile';
-// import PageHome from './Pages/Home/PageHome';
+import SplashPage from './Pages/Splash/SplashPage';
+import AppRoutes from './Router/AppRoutes';
 
 const GlobalStyle = createGlobalStyle`
 
@@ -28,7 +26,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      {/* <Footer /> */}
+      <AppRoutes />
     </>
   );
 }
