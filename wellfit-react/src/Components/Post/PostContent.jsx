b9848d7bcd5b62c2b4d51bab039b7240ac75e873
swJaNG12 <cbdjej38483@gmail.com>
2023-10-31 17:39:32 +0900

    ✨ 댓글 작성 기능 구현
    postFooter에서 댓글을 작성후 버튼 누르면 댓글목록이 리렌더되고 댓글이 추가된다.

--- wellfit-react/src/Components/Post/PostContent.jsx
+++ wellfit-react/src/Components/Post/PostContent.jsx
@@ -1,43 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PostCommentList from './PostCommentList';
 import PostFeed from './PostFeed';
 import PostFooter from './PostFooter';
 import { styled } from 'styled-components';
+import { getPostCommentList } from '../../api/GETPostCommentList';
+import { getMyInfo } from '../../api/PostMyInfo';
+import { useParams } from 'react-router-dom';
 
 const StyledHr = styled.hr`
   border: 1px solid #dbdbdb;
   margin: 0;
 `;
-const initialData = [
-  {
-    id: 0,
-    userName: '웰핏',
-    content: '오운완',
-    profileImage:
-      'https://images.unsplash.com/photo-1697694748470-5d9fc99a1b6a?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    date: new Date(),
-  },
-  {
-    id: 1,
-    userName: '한라봉',
-    content: '감귤주스',
-    profileImage:
-      'https://images.unsplash.com/photo-1697556399518-89cb479bfe52?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    date: new Date(),
-  },
-];
+
 export default function PostContent({ modalHandler, currentPostDetail }) {
-  const [commentList, setCommentList] = useState(initialData);
-  const addComment = (data) => {
-    const copyCommentList = [...commentList];
-    const newComment = data;
-  };
+  const params = useParams();
+  const [commentList, setCommentList] = useState(null);
+  const [myProfile, setMyProfile] = useState(null);
+
+  useEffect(() => {
+    const getCommentList = async () => {
+      const comments = await getPostCommentList(params.postid);
+      setCommentList(comments.comments);
+    };
+    getCommentList();
+
+    const getMyProfile = async () => {
+      const profile = await getMyInfo();
+      setMyProfile(profile);
+    };
+    getMyProfile();
+  }, []);
+
+  useEffect(() => {
+    console.log(commentList);
+  }, [commentList]);
+
+  useEffect(() => {
+    console.log(myProfile);
+  }, [myProfile]);
+
   return (
     <main>
       <PostFeed currentPostDetail={currentPostDetail} />
       <StyledHr />
       <PostCommentList commentList={commentList} modalHandler={modalHandler} />
-      <PostFooter addComment={addComment} />
+      <PostFooter myProfile={myProfile} setCommentList={setCommentList} />
     </main>
   );
 }
