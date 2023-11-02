8cb0131d2e422319ac1ad3a9dc9adf1e55b9eed2
swJaNG12 <cbdjej38483@gmail.com>
2023-11-02 13:03:17 +0900

    💄 style : 스크롤 커스텀 스타일 적용
    home page와 post page, post page의
    댓글리스트의 스크롤 스타일을 변경했습니다.

--- wellfit-react/src/Components/Home/HomeContent.jsx
+++ wellfit-react/src/Components/Home/HomeContent.jsx
@@ -3,11 +3,27 @@ import { styled } from 'styled-components';
 import HomeFeedList from './HomeFeedList';
 
 const StyledHomePageContent = styled.main`
-  padding: 20px 16px;
+  padding: 20px 16px 30px;
 
   /* 게시물 많아지면 스크롤 생성 */
   overflow: scroll;
   height: 100%;
+
+  /* Chrome, Safari */
+  &::-webkit-scrollbar {
+    /* display: none; */ /* 스크롤 바 숨기기 */
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: darkgrey; /* 스크롤 바 색상 */
+    border-radius: 10px; /* 스크롤 바 모서리 둥글게 */
+  }
+  &::-webkit-scrollbar-track {
+    background-color: transparent; /* 스크롤 바 배경 색상 */
+  }
+  /* Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: darkgrey transparent; /* 스크롤 바 색상, 스크롤 바 배경 색상 */
 `;
 
 export default function HomeContent({ followedUserFeedList }) {
--- wellfit-react/src/Components/Post/PostCommentList.jsx
+++ wellfit-react/src/Components/Post/PostCommentList.jsx
@@ -6,10 +6,25 @@ const StyledPostCommentList = styled.section`
   padding: 20px 16px;
   height: 235px;
   overflow-y: scroll;
+
+  /* Chrome, Safari */
+  &::-webkit-scrollbar {
+    /* display: none; */ /* 스크롤 바 숨기기 */
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: darkgrey; /* 스크롤 바 색상 */
+    border-radius: 10px; /* 스크롤 바 모서리 둥글게 */
+  }
+  &::-webkit-scrollbar-track {
+    background-color: transparent; /* 스크롤 바 배경 색상 */
+  }
+  /* Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: darkgrey transparent; /* 스크롤 바 색상, 스크롤 바 배경 색상 */
 `;
 
 export default function PostCommentList({ commentList, modalHandler }) {
-  console.log(commentList);
   if (!commentList) {
     return <div>Loading...</div>;
   }
