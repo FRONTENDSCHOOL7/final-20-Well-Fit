258dcf174b82e51edaed3fa94c4020ae0c7a4113
swJaNG12 <cbdjej38483@gmail.com>
2023-11-02 10:15:10 +0900

    ✨ feat: 홈 피드 이미지 처리
    - 홈에서 피드 이미지가 없으면 img wrapper는 렌더링되지 않습니다.
    - 피드 이미지가 여러개가 있다면 일렬로 쭉 렌더링됩니다.

--- wellfit-react/src/api/GETFollowedFeedList.jsx
+++ wellfit-react/src/api/GETFollowedFeedList.jsx
@@ -5,7 +5,6 @@ export const getFollowedUserFeedList = async () => {
 
   try {
     const response = await authInstance.get(URL);
-    console.log(response);
     if (response.data.status === 404 || response.status === 404) {
       throw new Error('404 에러');
     }
