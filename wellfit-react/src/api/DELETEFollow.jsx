f4bcb3e8639fa95c6514110d9a2f6046a52a6c9e
swJaNG12 <cbdjej38483@gmail.com>
2023-11-06 16:45:57 +0900

    ✨ feat(api/DELETEFollow) : 언팔로우 api 통신 인스턴스

---
+++ wellfit-react/src/api/DELETEFollow.jsx
@@ -0,0 +1,12 @@
+import { authInstance } from './instance';
+
+export const unFollow = async (accountname) => {
+  const UN_FOLLOW_URL = `profile/${accountname}/unfollow`;
+
+  try {
+    const response = await authInstance.get(UN_FOLLOW_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    console.log(response);
+  } catch (error) {}
+};
