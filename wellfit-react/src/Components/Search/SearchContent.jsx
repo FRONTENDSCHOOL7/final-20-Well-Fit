c4cc1038a07aabfd19e56770951b3beb0d82440b
swJaNG12 <cbdjej38483@gmail.com>
2023-11-06 10:25:45 +0900

    💄 style(SearchContent) : 유저 검색 페이지 스크롤 스타일 변경

--- wellfit-react/src/Components/Search/SearchContent.jsx
+++ wellfit-react/src/Components/Search/SearchContent.jsx
@@ -7,12 +7,28 @@ const StyledSearchContent = styled.main`
 
   overflow: scroll;
   height: 100%;
+
+  /* Chrome, Safari */
+  &::-webkit-scrollbar {
+    /* display: none; */ /* 스크롤 바 숨기기 */
+    width: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: darkgrey; /* 스크롤 바 색상 */
+    border-radius: 10px; /* 스크롤 바 모서리 둥글게 */
+  }
+  &::-webkit-scrollbar-track {
+    background-color: transparent; /* 스크롤 바 배경 색상 */
+  }
+  /* Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: darkgrey transparent; /*  스크롤 바 색상, 스크롤 바 배경 색상 */
 `;
 
-export default function SearchContent() {
+export default function SearchContent({ keyword }) {
   return (
     <StyledSearchContent>
-      <SearchUserList />
+      <SearchUserList keyword={keyword} />
     </StyledSearchContent>
   );
 }
