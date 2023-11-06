9a081e10dd6b49ffab0730e3246c7c8a54d8cd33
swJaNG12 <cbdjej38483@gmail.com>
2023-11-06 10:28:26 +0900

    ✨ feat(SearchUSerList) : api 통신 연결
    - 유저의 정보를 서버에 요청하는 getSearchList를 keyword값을 이용해서 유저 리스트를 받아온다.
    - 받아온 유저 리스트는 SearchUSerItem의 props로 보낸다.

--- wellfit-react/src/Components/Search/SearchUserList.jsx
+++ wellfit-react/src/Components/Search/SearchUserList.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useEffect } from 'react';
+import { getSearchList } from '../../api/GETSearch';
 import SearchUserItem from './SearchUserItem';
 
-export default function SearchUserList() {
-  return (
-    <ol>
-      <SearchUserItem />
-      <SearchUserItem />
-    </ol>
-  );
+export default function SearchUserList({ keyword }) {
+  const [searchList, setSearchList] = useState(null);
+
+  console.log(keyword);
+  useEffect(() => {
+    const getUserList = async () => {
+      console.log(keyword === '');
+      if (keyword === '') {
+        setSearchList(null);
+        return;
+      }
+
+      try {
+        const userList = await getSearchList(keyword);
+
+        console.log(userList);
+        setSearchList(userList);
+      } catch (error) {
+        console.error();
+      }
+    };
+    getUserList();
+  }, [keyword]);
+
+  const searchUserList = () => {
+    if (!searchList) {
+      return <div>Loading...</div>;
+    }
+    return searchList.map((user) => {
+      return <SearchUserItem user={user} key={user._id} keyword={keyword} />;
+    });
+  };
+
+  const renderUserList = searchUserList();
+
+  return <ol>{renderUserList}</ol>;
 }
