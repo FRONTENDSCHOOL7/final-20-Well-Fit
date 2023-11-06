ca3ed370176d7c341091e62380c6b02390aa470e
swJaNG12 <cbdjej38483@gmail.com>
2023-11-06 16:48:04 +0900

    ✨ feat : follow 리스트 페이지 api연결, 라우터 연결
    나의 팔로우 리스트 데이터를 api 통신을 통해 받아와 화면에 렌더링합니다.
    프로필의 이미지를 클릭하면 해당 유저의 프로필 페이지로 이동합니다.

--- wellfit-react/src/api/DELETEFollow.jsx
+++ wellfit-react/src/api/DELETEFollow.jsx
@@ -7,6 +7,7 @@ export const unFollow = async (accountname) => {
     const response = await authInstance.get(UN_FOLLOW_URL, {
       headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
     });
-    console.log(response);
+    console.log(response.data);
+    return response.data;
   } catch (error) {}
 };
